a16287b5add271f73426a650f99b3dc1f5986c96
Oussail <oussail.alaoui@gmail.com>
2017-12-23 19:08:23 +0000

    add preferred shops function

--- app/Http/Controllers/ShopsController.php
+++ app/Http/Controllers/ShopsController.php
@@ -21,9 +21,15 @@ class ShopsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+
+    public function nearby_index()
     {
         return view('nearby_shops');
+    }
+
+    public function preferred_index()
+    {
+        return view('preferred_shops');
     }
 
     //Function that return nearby shops
@@ -37,6 +43,18 @@ class ShopsController extends Controller
         $qry = 'SELECT id,shop_name,shop_description, SQRT(POW(69.1 * (lat - '.$lat.'), 2) +POW(69.1 * ('.$lng.' - lng) * COS(lat / 57.3), 2)) AS distance FROM t_shop HAVING distance < 25 and id not in (select shop_id from t_disliked WHERE TIMESTAMPDIFF(HOUR, unliked, NOW()) > 2 ) ORDER BY distance';
 
         $shops = DB::select($qry);
-    return response()->json($shops);
+        return response()->json($shops);
     } 
+    //Function that return preferred shops
+    public function preferredShop(Request $request)
+    {
+        //get user id
+        $id = \Auth::user()->id;
+
+        //select favorites shops for current user
+        $qry = 'SELECT id,shop_name,shop_description FROM t_shop WHERE id in (select shop_id from t_liked where user_id='.$id.')';
+
+        $shops = DB::select($qry);
+    return response()->json($shops);
+    }
 }
