d21539aeb362aecbd1b951fb5197624dc11cc538
Oussail <oussail.alaoui@gmail.com>
2017-12-23 18:14:19 +0000

    Make function that return nearby shops for angular js

--- app/Http/Controllers/ShopsController.php
+++ app/Http/Controllers/ShopsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use DB;
 class ShopsController extends Controller
 {
     /**
@@ -25,4 +25,18 @@ class ShopsController extends Controller
     {
         return view('nearby_shops');
     }
+
+    //Function that return nearby shops
+    public function getShops(Request $request)
+    {
+        //Get coordinates
+        $lat = (string)$request->input('lat');
+        $lng = (string)$request->input('lng');
+
+        //Make Query to Calculate distance
+        $qry = 'SELECT id,shop_name,shop_description, SQRT(POW(69.1 * (lat - '.$lat.'), 2) +POW(69.1 * ('.$lng.' - lng) * COS(lat / 57.3), 2)) AS distance FROM t_shop HAVING distance < 25 and id not in (select shop_id from t_disliked WHERE TIMESTAMPDIFF(HOUR, unliked, NOW()) > 2 ) ORDER BY distance';
+
+        $shops = DB::select($qry);
+    return response()->json($shops);
+    } 
 }
