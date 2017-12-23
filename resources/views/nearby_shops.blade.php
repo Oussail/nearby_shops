331fdcdc10498ad6c8224f89f155af03f93dd7d8
Oussail <oussail.alaoui@gmail.com>
2017-12-23 18:23:08 +0000

    add balise angular components for listing

--- resources/views/nearby_shops.blade.php
+++ resources/views/nearby_shops.blade.php
@@ -1,13 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="col-md-3">
-                
-            </div>
-        </div>
-    </div>
+<div class="row">
+<div class="container" ng-app="NearbyShops">
+	<div class="col-md-12" ng-controller="ShopsController">
+		<div class="col-md-3" ng-repeat="shop in shops">
+			<div class="card" style="width: 20rem;" >
+  				<img class="card-img-top img-responsive" src="{{ asset('Images/pic.png') }}" alt="">
+  				<div class="card-body">
+  				@verbatim
+    				<h4 class="card-title">{{ shop.shop_name }}</h4>
+    				<p class="card-text">{{ shop.shop_description }}</p>
+    			@endverbatim
+  				</div>
+			</div>
+			<br>
+		</div>
+	</div>
 </div>
 @endsection
