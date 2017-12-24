f00c770698dd26db0973992d8335ecdbfd1474e1
Oussail <oussail.alaoui@gmail.com>
2017-12-24 15:32:25 +0000

    giving id to every shop

--- resources/views/nearby_shops.blade.php
+++ resources/views/nearby_shops.blade.php
@@ -6,14 +6,14 @@
 <div class="container" ng-app="NearbyShops">
 	<div class="col-md-12" ng-controller="ShopsController">
 		<div class="col-md-3" ng-repeat="shop in shops">
-			<div class="card" style="width: 20rem;" >
-  				<img class="card-img-top img-responsive" src="{{ asset('Images/pic.png') }}" alt="">
+			<div id="shop-@verbatim{{ shop.id }}@endverbatim" class="card" style="width: 20rem;" >
+  				<img class="card-img-top img-responsive" src="{{ asset('Images/pic@verbatim{{ shop.id }}@endverbatim.png') }}" alt="">
   				<div class="card-body">
   				@verbatim
   				{{ url }}
     				<h4 class="card-title">{{ shop.shop_name }}</h4>
     				<p class="card-text">{{ shop.shop_description }}</p>
-    				<a href="#" class="btn btn-primary" ng-click="OpinionShop(shop.id,1)">Like</a> <a href="#" class="btn btn-danger" ng-click="OpinionShop(shop.id,0)">Dislike</a>
+    				<a href="#" class="btn btn-primary" ng-click="likeShop(shop.id)">Like</a> <a href="#" class="btn btn-danger" ng-click="dislikeShop(shop.id)">Dislike</a>
     			@endverbatim
   				</div>
 			</div>
--- resources/views/preferred_shops.blade.php
+++ resources/views/preferred_shops.blade.php
@@ -5,8 +5,8 @@
 <div class="container" ng-app="NearbyShops">
 	<div class="col-md-12" ng-controller="PreferredShopController">
 		<div class="col-md-3" ng-repeat="shop in f_shops">
-			<div class="card" style="width: 20rem;" >
-  				<img class="card-img-top img-responsive" src="{{ asset('Images/pic.png') }}" alt="">
+			<div id="shop-@verbatim{{ shop.id }}@endverbatim" class="card" style="width: 20rem;" >
+  				<img class="card-img-top img-responsive" src="{{ asset('Images/pic@verbatim{{ shop.id }}@endverbatim.png') }}" alt="">
   				<div class="card-body">
   				@verbatim
     				<h4 class="card-title">{{ shop.shop_name }}</h4>
