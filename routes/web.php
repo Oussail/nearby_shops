f00fa8ca21b09abacb913bb1610a33954d4d002a
Oussail <oussail.alaoui@gmail.com>
2017-12-24 15:16:27 +0000

    separate route for like and dislike

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,8 @@ Route::get('/api/v1/get_shops', 'ShopsController@getShops');
 Route::get('/my-preferred-shops', 'ShopsController@preferred_index')->name('preferred_shop');
 Route::get('/api/v1/preferred_shop', 'ShopsController@preferredShop');
 
-Route::get('/api/v1/user_opinion', 'ShopsController@user_opinion');
+Route::get('/api/v1/like_shop', 'ShopsController@LikeShop');
+Route::get('/api/v1/dislike_shop', 'ShopsController@DislikeShop');
 Route::get('/api/v1/remove_shop', 'ShopsController@remove_shop');
 
 
