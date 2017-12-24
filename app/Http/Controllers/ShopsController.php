7ff34fd6f3cc7a3807b9b7739cf8361615738b08
Oussail <oussail.alaoui@gmail.com>
2017-12-24 15:15:39 +0000

    separate like and dislike function + fixing some querys

--- app/Http/Controllers/ShopsController.php
+++ app/Http/Controllers/ShopsController.php
@@ -40,8 +40,13 @@ class ShopsController extends Controller
         $lng = (string)$request->input('lng');
 
         //Make a Query to Calculate distance
-        $qry = 'SELECT id,shop_name,shop_description, SQRT(POW(69.1 * (lat - :lat), 2) +POW(69.1 * (:lng - lng) * COS(lat / 57.3), 2)) AS distance FROM t_shop HAVING distance < 25 and id not in (select shop_id from t_disliked WHERE TIMESTAMPDIFF(HOUR, unliked, NOW()) > 2 ) and  id not in (select shop_id from t_liked) ORDER BY distance';
-
+        $qry = 'SELECT id,shop_name,shop_description, SQRT(POW(69.1 * (lat - :lat), 2) +POW(69.1 * (:lng - lng) * COS(lat / 57.3), 2)) AS distance FROM t_shop HAVING distance < 25 and id not in (select shop_id from t_disliked WHERE TIMESTAMPDIFF(HOUR,unliked, NOW()) < 2 ) and  id not in (select shop_id from t_liked) ORDER BY distance';
+        //Another way to do it
+        //$shops = DB::table('t_shop')
+        //        ->select('id,shop_name,shop_description', DB::raw('SQRT(POW(69.1 * (lat - :lat), 2) +POW(69.1 * (:lng - lng) * COS(lat // 57.3), 2)) AS distance'))
+        //        ->orderBy('distance')
+        //       ->havingRaw('distance < 25 and id not in (select shop_id from t_disliked WHERE TIMESTAMPDIFF(HOUR,unliked, NOW()) < 2 ) and  id not in (select shop_id from t_liked)')
+        //        ->get();
         $shops = DB::select($qry,['lat' => $lat,'lng' => $lng]);
         return response()->json($shops);
     } 
@@ -50,30 +55,37 @@ class ShopsController extends Controller
     {
         //get user id
         $id = \Auth::user()->id;
-
         //select favorites shops for current user
         $shops = DB::select('SELECT id,shop_name,shop_description FROM t_shop WHERE id in (select shop_id from t_liked where user_id= :id)', ['id' => $id]);
-
     return response()->json($shops);
     }
 
     //Function make user opinion by like or dislike a shop 
-    public function user_opinion(Request $request)
+    public function LikeShop(Request $request)
     {
         //retrieve user id
         $user_id = \Auth::user()->id;
         //get shop id
         $shop_id = (string)$request->input('shop_id');
-        //get opinion*
-        $opinion = (string)$request->input('opinion');
-        if($opinion == 1)
-        {
+
         DB::delete('delete from t_disliked where shop_id=:id1 and user_id=:id2',['id1' => $shop_id,'id2' => $user_id]);
         DB::insert('insert into t_liked (liked,user_id,shop_id) values (NOW(), ?, ?)', [$user_id, $shop_id]);
-        }else if($opinion == 0)
+    }
+    //Function make user opinion by like or dislike a shop 
+    public function DislikeShop(Request $request)
+    {
+        //retrieve user id
+        $user_id = \Auth::user()->id;
+        //get shop id
+        $shop_id = (string)$request->input('shop_id');
+        //i add this try-catch clause because after 2 hours the dislike shops will appear again in the main page so i will update the unliked time again
+        try
         {
-        DB::delete('delete from t_liked where shop_id=:id1 and user_id=:id2',['id1' => $shop_id,'id2' => $user_id]);
-        DB::insert('insert into t_disliked (unliked,user_id,shop_id) values (NOW(), ?, ?)', [$user_id, $shop_id]);
+            DB::insert('insert into t_disliked (unliked,user_id,shop_id) values (NOW(), ?, ?)', [$user_id, $shop_id]);
+            DB::delete('delete from t_liked where shop_id=:id1 and user_id=:id2',['id1' => $shop_id,'id2' => $user_id]);
+        }catch(Exception $e)
+        {
+            DB::update('update t_disliked set unliked=NOW() where shop_id = ? and user_id = ?', [$user_id, $shop_id]);
         }
     }
     //Function that  remove shop from preferred shop page
