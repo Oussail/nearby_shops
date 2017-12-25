0206dd07c682c60bc69e9e44da67fcfa63fc4656
Oussail <oussail.alaoui@gmail.com>
2017-12-25 08:01:41 +0000

    add unsinged type to foreign keys but still didn't work

--- database/migrations/2017_12_22_193609_create_shops_table.php
+++ database/migrations/2017_12_22_193609_create_shops_table.php
@@ -13,7 +13,7 @@ class CreateShopsTable extends Migration
      */
     public function up()
     {
-       /*Schema::create('t_shop', function (Blueprint $table) {
+       Schema::create('t_shop', function (Blueprint $table) {
             $table->increments('id');
             $table->string('shop_name');
             $table->string('shop_description');
@@ -24,20 +24,20 @@ class CreateShopsTable extends Migration
         });
        Schema::create('t_liked', function (Blueprint $table) {
             $table->dateTime('liked');
-            $table->smallInteger('user_id');
-            $table->smallInteger('shop_id');
+            $table->smallInteger('user_id')->unsigned();
+            $table->smallInteger('shop_id')->unsigned();
             $table->primary(array('user_id', 'shop_id'));
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('shop_id')->references('id')->on('t_shop')->onDelete('cascade');
         });
        Schema::create('t_disliked', function (Blueprint $table) {
             $table->dateTime('unliked');
-            $table->smallInteger('user_id');
-            $table->smallInteger('shop_id');
+            $table->smallInteger('user_id')->unsigned();
+            $table->smallInteger('shop_id')->unsigned();
             $table->primary(array('user_id', 'shop_id'));
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('shop_id')->references('id')->on('t_shop')->onDelete('cascade');
-        });*/
+        });
     }
 
     /**
